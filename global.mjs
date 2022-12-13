c98a175566732d8d5b7c9921674fb85a219e6a45
Thon Ly <thonly@heartbank.ceo>
2022-12-12 20:07:55 -0800

    refactor

--- global.mjs
+++ global.mjs
@@ -3,18 +3,3 @@ export const THONLY = window.location.hostname === '127.0.0.1' ? "http://127.0.0
 export const UNIVERSITY = window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5610" : "https://siliconwat.com";
 
 export const BACKGROUND = "radial-gradient(circle at bottom left, hsla(161, 80%, 45%, 1) 0%, hsla(215, 80%, 45%, 1) 100%)";
-
-export async function getData(url) {
-    let cache = localStorage.getItem(url);
-    if (cache) {
-        return JSON.parse(cache);
-    } else {
-        try {
-            cache = await (await fetch(url, { cache: "no-store" })).json();
-        } catch(error) {
-            cache = {};
-        }
-        localStorage.setItem(url, JSON.stringify(cache))
-        return cache;
-    }  
-}
