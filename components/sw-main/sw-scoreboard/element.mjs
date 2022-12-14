421dc3f1e7828e0d6d060436f17c5fe608cafcc8
Thon Ly <thonly@heartbank.ceo>
2022-12-13 17:53:39 -0800

    begin tables

--- components/sw-main/sw-scoreboard/element.mjs
+++ components/sw-main/sw-scoreboard/element.mjs
@@ -1,3 +1,4 @@
+import { FRONTEND } from "/global.mjs";
 import template from './template.mjs';
 
 class SwScoreboard extends HTMLElement {
@@ -16,7 +17,19 @@ class SwScoreboard extends HTMLElement {
     render(cohort, game) {
         this.shadowRoot.getElementById('cohort').textContent = this.#cohorts[cohort];
         this.shadowRoot.getElementById('best').textContent = game ? game.capitalize() : "Student Roster";
+
+        this.#render(cohort, game);
         this.style.display = 'block';
+    }
+
+    async #render(cohort, game) {
+        const { getYear, getTerm, getData } = await import(`${FRONTEND}/global2.mjs`);
+        const y = getYear();
+        const term = getTerm();
+        const students = await getData(`https://raw.githubusercontent.com/SiliconWat/${cohort}-cohort/main/Students.json`);
+        const votes = await getData(`https://raw.githubusercontent.com/SiliconWat/${cohort}-cohort/main/${y}/${term[1] === 'semester' ? "Semesters" : "Quarters"}/${term[2].capitalize()}/Votes.json`);
+        console.log(students)
+        console.log(votes)
     }
 }
 
--- components/sw-main/sw-scoreboard/template.mjs
+++ components/sw-main/sw-scoreboard/template.mjs
@@ -5,6 +5,29 @@ template.innerHTML = `
     <main>
         <h1 id="cohort"></h1>
         <h2 id="best"></h2>
+        <table>
+            <caption>Students vs Chapters</caption>
+            <colgroup>
+                <col span="1">
+                <col span="1">
+                <col span="1">
+            </colgroup>
+            <thead>
+                <tr>
+                    <th scope="col">CHAPTER</th>
+                    <th scope="col">@thonly</th>
+                    <th scope="col">AVG</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+            <tfoot>
+                <tr>
+                    <th scope="row">SCORE</th>
+                    <th scope="col">100%</th>
+                    <th scope="row">Overall Cohort Score</th>
+                </tr>
+            </tfoot>
+        </table>
     </main>
 `;
 
