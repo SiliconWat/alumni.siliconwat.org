95dcca8df110fe4f03eeeb6bd6d4937e3828288c
Thon Ly <thonly@heartbank.ceo>
2022-12-30 13:52:48 -0800

    refactor

--- components/sw-main/element.mjs
+++ components/sw-main/element.mjs
@@ -20,7 +20,7 @@ class SwMain extends HTMLElement {
         this.shadowRoot.querySelector("slot").assignedElements().find(element => element.tagName === this.#hash[0]).render(this.#hash[1], this.#hash[2]);
         document.documentElement.style.backgroundImage = BACKGROUND;
         document.querySelector('main').style.display = 'flex';
-        this.scrollIntoView({ behavior: "smooth", block: "start", inline: "center" });
+        document.body.scrollIntoView({ behavior: "smooth", block: "start", inline: "center" });
     }
 
     get #hash() {
